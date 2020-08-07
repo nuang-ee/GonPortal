50cd100d9665447a88aa5f661eabf393b64b5081
cs20180603 <random0411@kaist.ac.kr>
2020-08-07 00:08:57 -0700

    removed unnecessary files, arranged directories

---
+++ Server/Controllers/AccountController.ts
@@ -0,0 +1,163 @@
+import * as express from "express";
+import asyncHandler from "express-async-handler";
+import { NewbieAccount } from "../utils/db";
+import { sendAuthMail, verifyAuthMail } from "../utils/mailAuth";
+import { Auth } from "../Core/Auth";
+import * as Mongoose from "mongoose";
+import { INewbieAccount } from "../Documents/AccountDocument";
+
+export const AccountControlRouter = express.Router();
+
+// FIXME: replace all response send, with proper vue.js actions.
+
+/* NEW ACCOUNT */
+AccountControlRouter.post('/register', asyncHandler(async (req, res) => {
+    const { uid, password, sNum, name, phoneNum } = req.body;
+    let email = req.body.email;
+    if ( !uid || !password || !sNum || !name || !email || !phoneNum ) {
+        return res.status(400).send("Invalid Request");
+    }
+
+    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!emailRegex.test(email)) {
+        return res.status(400).send("Invalid Email Form");
+    }
+
+    /* Only email domain is case insensitive per RFC 5321.
+     * However many email service providers (including mail.kaist.ac.kr) also consider
+     * user name as case insensitive - we follow this rule. */
+    email = email.toLowerCase();
+
+    const whiteList: string[] = ["kaist.ac.kr"];
+    const emailDomain = email.split("@")[1];
+    if (!whiteList.includes(emailDomain)) {
+        return res.status(400).send("Sorry, we only allow KAIST students.");
+    }
+
+    // FIXME : edit response to show some toast popup or something, instead of raw text.
+    if (await NewbieAccount.countDocuments({ uid: uid }) > 0) {
+        return res.status(400).send("Username Already Exists");
+    }
+
+    if (await NewbieAccount.countDocuments({ sNum: sNum }) > 0) {
+        return res.status(400).send("Already Registered Student");
+    }
+
+    if (await NewbieAccount.countDocuments({ email: email }) > 0) {
+        return res.status(400).send("Already Registered email address");
+    }
+
+    const pwHash = await Auth.hash(req.body.password);
+
+    const account = await NewbieAccount.create({
+        uid: uid,
+        password: pwHash,
+        sNum: sNum,
+        name: name,
+        email: email,
+        emailAuthed: false,
+        phoneNum: phoneNum,
+        resume: "",
+        solved: 0,
+        created: Date.now()
+    })
+
+    sendAuthMail(account._id, email, (err, info) => { if (err) console.log(err, info) });
+    
+    return res.status(200).send("<p>Successfully added your account! Check your email to authenticate your account.</p>");
+}));
+
+AccountControlRouter.get('/auth/mail/resend', asyncHandler(async (req, res) => {
+    if (!req.session) {
+        return res.status(400).send("You haven't logged in");
+    }
+
+    const _id = req.session._id;
+    if (!_id) return res.status(400).send("Invalid Session");
+
+    const authed = req.session.Authed;
+    if (authed) return res.status(200).send("Your Account has been already authorized!");
+
+    const user = await NewbieAccount.findOne({ _id: _id });
+    if (!user) return res.status(400).send("Invalid Session");
+    if (user.emailAuthed) return res.status(200).send("Your Account has been already authorized!");
+
+    sendAuthMail(_id, user.email, (err, info) => {if (err) console.log(err, info)});
+
+    return res.status(200).send("<p>Sent Authentication Email! Check your email to authenticate your account.</p>");
+}));
+
+/* MAIL AUTHENTICATION */
+AccountControlRouter.get('/auth/mail/verify', asyncHandler(async (req, res) => {
+    const { iv, authCode, authTag } = req.query;
+
+    if (typeof iv !== 'string' || typeof authCode !== 'string' || typeof authTag !== 'string') {
+        return res.status(400).send("Mail authentication failed. Please check the verification link or resend verification mail.");
+    }
+    const authData = verifyAuthMail(iv as string, authCode as string, authTag as string);
+    if (authData === undefined) {
+        return res.status(400).send("Mail authentication failed. Please check the verification link or resend verification mail.");
+    }
+
+    const account = await NewbieAccount.findById(authData._id);
+    if (account === null) {
+        return res.status(400).send("Mail authentication failed. Please check the verification link or resend verification mail.");
+    } else if (account.email !== authData.email) {
+        return res.status(400).send("Mail authentication failed. Your email has been changed, please resend verification mail.");
+    } else if (account.emailAuthed) {
+        return res.status(400).send("Mail already authenticated.");
+    }
+    account.emailAuthed = true;
+
+    await account.save();
+
+    return res.status(200).send("Mail authenticated.");
+}));
+
+/* UPDATE ACCOUNT INFORMATION */
+AccountControlRouter.put('/update/:id', asyncHandler(async (req, res) => {
+    // TODO: add validation logic
+    await NewbieAccount.findByIdAndUpdate(req.params.id, req.body, {new: true});
+    res.status(200).send("Succeed to Update Account");
+}));
+
+/* LOGIN */
+AccountControlRouter.post('/auth/login', asyncHandler(async (req, res) => {
+    const { uid, password } = req.body;
+
+    if (req.session?._id) {
+        return res.send('<h1>already Logged In</h1>');
+    } else {
+        const user = await NewbieAccount.findOne({uid: uid});
+        if (!user?.password || !(await Auth.isValid(password, user.password)))
+            return res.send("invalid username or password");
+        
+        if (!req.session) return res.status(400).send("invalid request");
+
+        req.session._id = user._id;
+        req.session.Authed = user.emailAuthed;
+        req.session.LoggedIn = true;
+
+        // req.session.save is done implicitly, on res.send
+        // FIXME : redirect to main page.
+        return res.send("you have logged in");
+    }
+}));
+
+/* LOGOUT */
+AccountControlRouter.get('/auth/logout', asyncHandler(async (req, res) => {
+    if (!req.session) return res.status(400).send("You haven't logged in");
+
+    const _id = req.session._id;
+    if (!_id) return res.status(400).send("Invalid Session");
+
+    req.session.destroy((err) => {
+        if (err) {
+            console.log("error on destroying session");
+            return res.status(500).send("Error on Logout");
+        }
+        // if needed,
+        // res.clearCookie('connect.sid');
+        return res.status(200).send("Successfully logged out");
+    })
+}))
---
+++ Server/utils/mailAuth.ts
@@ -0,0 +1,73 @@
+import * as nodemailer from "nodemailer";
+import * as crypto from "crypto";
+import * as querystring from "querystring";
+import b64u from "base64url";
+import { MAIL_CONFIG } from "../config";
+
+if (!MAIL_CONFIG.NODEMAILER_USERNAME || !MAIL_CONFIG.NODEMAILER_PASSWORD) {
+    console.log('env not set: nodemailer');
+    process.exit(1);
+}
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: true,
+    auth: {
+        user: MAIL_CONFIG.NODEMAILER_USERNAME,
+        pass: MAIL_CONFIG.NODEMAILER_PASSWORD,
+    },
+});
+
+const { secret, validDuration, ivLength, authTagLength } = MAIL_CONFIG;
+const key = crypto.createHash('sha256').update(secret).digest();
+
+export type AuthData = {
+    _id: string,
+    email: string,
+    expiresAt: Number,
+};
+
+export function sendAuthMail(_id: string, email: string, callback: (err: Error | null, info: any) => void) {
+    const authData = {
+        _id: _id,
+        email: email,
+        expiresAt: Date.now() + validDuration,
+    } as AuthData;
+
+    const iv = crypto.randomBytes(ivLength);
+    const authCipher = crypto.createCipheriv('aes-256-gcm', key, iv, {authTagLength: authTagLength});
+    const authCode = Buffer.concat([authCipher.update(JSON.stringify(authData)), authCipher.final()]); 
+    const authTag = authCipher.getAuthTag();
+
+    // TODO: compose link from config or env
+    const queryStr = querystring.stringify({iv: b64u.encode(iv), authCode: b64u.encode(authCode), authTag: b64u.encode(authTag)});
+    const verifyLink = `http://localhost/users/auth/mail/verify?${queryStr}`;
+    transporter.sendMail({
+        from: `"KAIST GoN" <noreply@goatskin.kr>`,
+        to: email,
+        subject: 'GoN Newbie Recruiting Page Authentication',
+        text: `Verification Link: ${verifyLink}`,
+        html: `<p>Verification Link: <a href="${verifyLink}">${verifyLink}</a></p>`,
+    }, callback);
+}
+
+export function verifyAuthMail(iv: string, authCode: string, authTag: string): AuthData | undefined {
+    if (!iv || !authCode || !authTag) {
+        return undefined;
+    }
+    try {
+        const authDecipher = crypto.createDecipheriv('aes-256-gcm', key, b64u.toBuffer(iv), {authTagLength: authTagLength});
+        authDecipher.setAuthTag(b64u.toBuffer(authTag));
+        const dataDecrypted = Buffer.concat([authDecipher.update(b64u.toBuffer(authCode)), authDecipher.final()]);
+        const authData = JSON.parse(dataDecrypted.toString()) as AuthData; 
+
+        if (authData.expiresAt < Date.now()) {
+            return undefined;  // expired
+        }
+        return authData;
+    } catch (_) {
+        return undefined;  // decipher failure
+    }
+}
