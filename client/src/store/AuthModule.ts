2c5652af540ac81ec6b36f27f57c7fa7a2f04caa
cs20180603 <random0411@kaist.ac.kr>
2020-08-17 21:09:31 -0700

    rename files to camelCase

--- client/src/store/AuthModule.ts
+++ client/src/store/AuthModule.ts
@@ -1,4 +1,4 @@
-import AuthService from "../services/auth_service";
+import AuthService from "../services/AuthService";
 import { Module } from 'vuex'
 import { register } from 'register-service-worker';
 
