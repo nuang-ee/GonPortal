45bed067e453c964b9155d8f6459f5f84b655111
cs20180603 <random0411@kaist.ac.kr>
2020-11-07 03:43:32 -0800

    replace vue-cookies-ts to vue-cookies...

--- client/src/main.ts
+++ client/src/main.ts
@@ -6,7 +6,7 @@ import store from "./store";
 import vuetify from "./plugins/vuetify";
 import { ValidationProvider } from 'vee-validate';
 import axios from 'axios';
-import VueCookies from "vue-cookies-ts";
+import VueCookies from "vue-cookies";
 
 Vue.component('ValidationProvider', ValidationProvider);
 Vue.use(VueCookies);
