82f84da209698280a74f3d4a43685a8c8384d5a3
Nuang_ee <50770626+nuang-ee@users.noreply.github.com>
2020-11-08 13:49:40 +0900

    Merge pull request #7 from bonjune/master
    야무진 머지

--- Server/Controllers/Recruitment/AccountController.ts
+++ Server/Controllers/Recruitment/AccountController.ts
@@ -6,61 +6,120 @@ import { Auth } from "../../Core/Auth";
 import { JWTAuth, IToken } from "../../Core/Recruitment/JWTAuth";
 import * as Mongoose from "mongoose";
 import { IApplicantAccount } from "../../Documents/Recruitment/AccountDocument";
+import {Query} from "mongoose";
 
 export const AccountControlRouter = express.Router();
+
+type ValidationSuccess = { isValid: true }
+type ValidationFailure = { isValid: false, code: number, error: string }
+type ValidatedResult =
+    | ValidationSuccess
+    | ValidationFailure
+type Validate = (...args: any[]) => ValidatedResult
+
+const validateNonEmptyFields: Validate = (body) => {
+    const { username, password, sNum, email, name, phoneNum } = body;
+    if (!username || !password || !sNum || !name || !email || !phoneNum) {
+        return {
+            isValid: false,
+            code: 400, // BAD_REQUEST
+            error: "Invalid Request"
+        };
+    }
+    return {
+        isValid: true
+    };
+};
+
+const validateEmail: Validate = (email: string, whiteList: string[]) => {
+    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!emailRegex.test(email)) {
+        return {
+            isValid: false,
+            code: 400, // BAD_REQUEST
+            error: "Invalid Email Form"
+        };
+    }
+    
+    const _email = email.toLowerCase()
+    const emailDomain = _email.split("@")[1]
+    if (!whiteList.includes(emailDomain)) {
+        return {
+            isValid: false,
+            code: 400, // BAD_REQUEST
+            error: "Only KAIST Domain is acceptable"
+        };
+    }
+    
+    return {
+        isValid: true
+    };
+};
+
+const validateAlreadyRegisteredDocs = async (username: string, sNum: string, email: string): Promise<ValidatedResult[]> => {
+    const checkList: [string, Query<number>][] = [
+        ["username", ApplicantAccount.countDocuments({ username })],
+        ["student number", ApplicantAccount.countDocuments({ sNum })],
+        ["email", ApplicantAccount.countDocuments({ email })],
+    ];
+    
+    const checkActions: Promise<ValidatedResult>[] = checkList.map(async checkItem => {
+        const [fieldName, query] = checkItem;
+        const isDuplicated = (await query) > 0
+        if (isDuplicated) {
+            return {
+                isValid: false,
+                code: 409, // CONFLICT : has a conflict to the current state of the server
+                error: `${fieldName} is already registered`
+            };
+        }
+        return { isValid: true } as ValidationSuccess;
+    });
+    
+    return Promise.all(checkActions)
+};
+
+type AccountControllerResponse = {
+    success: boolean,
+    message: string,
+}
 
 /* NEW ACCOUNT */
 AccountControlRouter.post(
     "/auth/register",
     asyncHandler(async (req, res) => {
-        const response = {
+        const response: AccountControllerResponse = {
             success: false,
             message: "",
         };
-
-        const { username, password, sNum, name, phoneNum } = req.body;
-        let email = req.body.email;
-        if (!username || !password || !sNum || !name || !email || !phoneNum) {
-            response.message = "invalid request";
-            return res.status(400).json(response);
-        }
-
-        const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        if (!emailRegex.test(email)) {
-            response.message = "invalid email form";
-            return res.status(400).json(response);
+        
+        const { username, password, sNum, name, email, phoneNum } = req.body;
+        const nonEmptyFieldsValidatedResult = validateNonEmptyFields(req.body)
+        if (!nonEmptyFieldsValidatedResult.isValid) {
+            response.message = nonEmptyFieldsValidatedResult.error
+            return res.status(nonEmptyFieldsValidatedResult.code).json(response);
         }
 
         /* Only email domain is case insensitive per RFC 5321.
          * However many email service providers (including mail.kaist.ac.kr) also consider
          * user name as case insensitive - we follow this rule. */
-        email = email.toLowerCase();
-
-        const whiteList: string[] = ["kaist.ac.kr"];
-        const emailDomain = email.split("@")[1];
-        if (!whiteList.includes(emailDomain)) {
-            response.message = "sorry, we only allow KAIST students.";
-            return res.status(200).json(response);
+        const whiteList = ["kaist.ac.kr"];
+        const emailValidatedResult = validateEmail(email, whiteList)
+        if (!emailValidatedResult.isValid) {
+            response.message = emailValidatedResult.error;
+            return res.status(emailValidatedResult.code).json(response);
         }
-
-        // FIXME : edit response to show some toast popup or something, instead of raw text.
-        if ((await ApplicantAccount.countDocuments({ username: username })) > 0) {
-            response.message = "username already exists";
-            return res.status(200).json(response);
+        
+        const alreadyRegisteredDocsValidatedResult = await validateAlreadyRegisteredDocs(username, sNum, email)
+        const duplicatedDocsValidatedResult = alreadyRegisteredDocsValidatedResult.filter(result => !result.isValid) as ValidationFailure[]
+        if (duplicatedDocsValidatedResult.length > 0) {
+            duplicatedDocsValidatedResult.forEach(failure => {
+                response.message += failure.error
+            });
+            return res.status(duplicatedDocsValidatedResult[0].code).json(response);
         }
-
-        if ((await ApplicantAccount.countDocuments({ sNum: sNum })) > 0) {
-            response.message = "student already registered";
-            return res.status(200).json(response);
-        }
-
-        if ((await ApplicantAccount.countDocuments({ email: email })) > 0) {
-            response.message = "email already registered";
-            return res.status(200).json(response);
-        }
-
+        
         const pwHash = await Auth.hash(req.body.password);
-
         const account = await ApplicantAccount.create({
             role: 'applicant',
             username: username,
@@ -76,7 +135,9 @@ AccountControlRouter.post(
         });
 
         sendAuthMail(account._id, email, (err, info) => {
-            if (err) console.log(err, info);
+            if (err) {
+                console.log(err, info);
+            }
         });
 
         response.success = true;
